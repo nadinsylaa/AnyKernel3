63f5746ae6729d22a8b74b811b3f1424d118aef0
BryanHafidz <nugroho31.com@gmail.com>
2020-07-16 00:17:51 -0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=BrynKernel By BryanHafidz
+#kernel.string=BrynKernel By BryanHafidz
 do.devicecheck=1
 do.modules=0
 do.systemless=1
